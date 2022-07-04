6658506d6be4e1ec93410bfb8fa96556a203c400
black-code1 <legrandmahamat1@gmail.com>
2022-07-04 16:37:35 +0100

    Practical Component Exercice #1:Message

--- main.js
+++ main.js
@@ -1,25 +1,24 @@
-Vue.component('task-list',{
-    template:`
-              <div>
-                <task v-for="task in tasks">{{task.task}}</task>
-              </div>
-`,
-
+Vue.component('message',{
+    props: ['title', 'body'],
     data(){
         return {
-            tasks: [
-                {task:'Go to the store', complete:true},
-                {task:'Go to the email', complete:false},
-                {task:'Go to the farm', complete:true},
-                {task:'Go to work', complete:false},
-            ]
-        }
-    }
-});
+            isVisible:true
+        };
+    },
+    template:`
+    <article class="message" v-show="isVisible">
+        <div class="message-header">
+            {{title}}
+            
+            <button class="button" @click="isVisible = false">x</button>
+        </div>
+        <div class="message-body">
+            {{body}}
+        </div>
+    </article>
+    `,
 
-Vue.component('task', {
-    template:`<li><slot></slot></li>`
-})
+});
 
 new Vue({
     el:'#root'
